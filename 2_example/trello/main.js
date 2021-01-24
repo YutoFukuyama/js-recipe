5003fd7a83af27c91559b3ac18a6a6d883d50004
福山悠斗 <yuto@fukuyamayuutonoMacBook-Pro.local>
2021-01-24 17:01:37 +0900

    todo

--- 2_example/trello/main.js
+++ 2_example/trello/main.js
@@ -1,6 +1,14 @@
 const input = document.getElementById("text")
 const button = document.getElementById("button")
 const display = document.getElementById("display")
+let todos=[]
+todos=JSON.parse(localStorage["todo"])
+console.log(todos)
+for(i=0;i<todos.length;i++){
+    const card=document.createElement("div")
+    card.textContent=todos[i]
+    display.append(card)
+}
 
 button.onclick = function(){
     
@@ -8,8 +16,12 @@ button.onclick = function(){
     
     }else{
         const card=document.createElement("div")
+        //const deleteButton=document.createElement("button")
         card.textContent = input.value
         display.append(card)
+        todos.push(input.value)
+        console.log(todos)
+        localStorage["todo"]=JSON.stringify(todos)
         input.value = ""
     }
 }
