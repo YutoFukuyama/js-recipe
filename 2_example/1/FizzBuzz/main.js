881efacdd08700ada37f106b2cb0a5a531fc6b2c
福山悠斗 <yuto@fukuyamayuutonoMacBook-Pro.local>
2021-01-31 12:17:47 +0900

    FizzBuzz

---
+++ 2_example/1/FizzBuzz/main.js
@@ -0,0 +1,18 @@
+
+const genkifunction = function(number){
+    for (let n=1; n<=number; n++) {
+         if (n % 15 ===0){
+            console.log ("fizzbuzz")
+    }
+    else if (n % 3 === 0 ) {
+        console.log ("fizz")}
+    
+    else if (n % 5 ===0){
+        console.log ("buzz")}
+    else {
+        console.log (n)
+    }
+    
+    }
+    }
+    genkifunction (100)
